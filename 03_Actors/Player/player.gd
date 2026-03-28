f455a5db6f3b562db2a8ba907fe7d25d6d397e41
EvesTwised <Stylesteph1992@outlook.com>
2026-03-28 18:36:08 -0500

    revert: Revert to stable placeholder due to incompatible model asset

--- 03_Actors/Player/player.gd
+++ 03_Actors/Player/player.gd
@@ -13,22 +13,19 @@ extends CharacterBody3D
 @export_group("Camera")
 @export var mouse_sensitivity: float = 0.1
 
-@onready var animation_tree = $AnimationTree
-@onready var state_machine = animation_tree.get("parameters/playback")
-@onready var pivot = $Pivot
-@onready var camera = $"Pivot/female_body_a-pose__t-pose_free/Armature/Skeleton3D/ShoulderBone/Camera3D"
-@onready var flashlight = $"Pivot/female_body_a-pose__t-pose_free/Armature/Skeleton3D/RightHandBone/Flashlight"
+@onready var head = $Head
+@onready var camera = $Head/Camera3D
+@onready var flashlight = $Head/Camera3D/Flashlight
 
 var _gravity: float
 
 func _ready() -> void:
 	_gravity = ProjectSettings.get_setting("physics/3d/default_gravity") * gravity_scale
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
-	animation_tree.active = true
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event is InputEventMouseMotion:
-		pivot.rotate_y(deg_to_rad(-event.relative.x * mouse_sensitivity))
+		head.rotate_y(deg_to_rad(-event.relative.x * mouse_sensitivity))
 		camera.rotation.x = clamp(camera.rotation.x + deg_to_rad(-event.relative.y * mouse_sensitivity), deg_to_rad(-80), deg_to_rad(80))
 
 	if event.is_action_just_pressed("flashlight"):
@@ -41,7 +38,7 @@ func _physics_process(delta: float) -> void:
 
 	# Movement
 	var input_dir = Input.get_vector("move_left", "move_right", "move_forward", "move_backward")
-	var direction = (pivot.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	var direction = (head.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	var is_sprinting = Input.is_action_pressed("sprint")
 	var speed = sprint_speed if is_sprinting else walk_speed
 
@@ -53,9 +50,5 @@ func _physics_process(delta: float) -> void:
 	else:
 		velocity.x = lerp(velocity.x, 0.0, deceleration)
 		velocity.z = lerp(velocity.z, 0.0, deceleration)
-
-	# Animation Control
-	var move_blend = velocity.length() / sprint_speed
-	animation_tree.set("parameters/BlendSpace2D/blend_position", move_blend)
 
 	move_and_slide()
