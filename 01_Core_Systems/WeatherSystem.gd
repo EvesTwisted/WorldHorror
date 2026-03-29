05e0d9595a06118c3df60157c45fe25d114bc028
EvesTwised <Stylesteph1992@outlook.com>
2026-03-28 20:24:07 -0500

    fix: modernize shader to Godot 4.5.2 standards

--- 01_Core_Systems/WeatherSystem.gd
+++ 01_Core_Systems/WeatherSystem.gd
@@ -1,6 +1,7 @@
 extends Node
 
 @onready var world_environment = get_tree().get_first_node_in_group("WorldEnvironment")
+@onready var glitch_effect = get_tree().get_root().find_child("GlitchEffect", true, false)
 
 func _ready() -> void:
 	var player = get_tree().get_first_node_in_group("Player")
@@ -16,3 +17,6 @@ func _on_stress_changed(new_stress: float) -> void:
 		else:
 			env.volumetric_fog_density = 0.01
 			env.background_energy_multiplier = 1.0
+
+	if glitch_effect and glitch_effect.material is ShaderMaterial:
+		glitch_effect.material.set_shader_parameter("stress", new_stress / 100.0)
